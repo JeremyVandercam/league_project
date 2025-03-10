8211873392a7781657c3778f3eee534382a155d6
Maximilian Gust <@users.noreply.github.com>
2025-03-10 17:28:33 +0100

    Created functions for uploading & downloading data from BQ

---
+++ ml_logic/data.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+from google.cloud import bigquery
+from params import GCP_PROJECT, BQ_DATASET, LOCAL_DATA_PATH
+from pathlib import Path
+
+
+
+
+def get_data_with_cache(
+    query:str,
+    cache_path:Path,
+    data_has_header=True):
+
+    """
+    Retrieve `query` data from BigQuery, or from `cache_path` if the file exists
+    Store at `cache_path` if retrieved from BigQuery for future use
+    """
+
+    if cache_path.is_file():
+        df = pd.read_csv(cache_path, header='infer' if data_has_header else None)
+    else:
+        client = bigquery.Client(project=GCP_PROJECT)
+        query_job = client.query(query)
+        result = query_job.result()
+        df = result.to_dataframe()
+
+    if df.shape[0] > 1:
+        df.to_csv(cache_path, header=data_has_header, index=False)
+
+    return df
+
+def upload_data_to_bq(
+    data:pd.DataFrame,
+    table:str,
+    truncate:bool
+    ):
+
+    """
+    - Save the DataFrame to BigQuery
+    - Empty the table beforehand if `truncate` is True, append otherwise
+    """
+
+    assert isinstance(data, pd.DataFrame)
+    full_table_name = f"{GCP_PROJECT}.{BQ_DATASET}.{table}"
+
+
+    client = bigquery.Client()
+
+    # Define write mode and schema
+    write_mode = "WRITE_TRUNCATE" if truncate else "WRITE_APPEND"
+    job_config = bigquery.LoadJobConfig(write_disposition=write_mode)
+
+    # Load data
+    job = client.load_table_from_dataframe(data, full_table_name, job_config=job_config)
+    result = job.result()
+
+
+
+
+
+if __name__ == "__main__":
+    data_query_cache_path = Path(LOCAL_DATA_PATH).joinpath("games2.csv")
+    df = get_data_with_cache(query="SELECT * FROM `league-project-lewagon.league_of_legends_dataset.game_data_kaggle` LIMIT 10",cache_path=data_query_cache_path)
+
+    upload_data_to_bq(data=df,table="test",truncate=True)
+
+    breakpoint()
---
+++ params.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+
+####################  VARIABLES  ##################
+GCP_PROJECT = os.environ["GCP_PROJECT"]
+BQ_DATASET = os.environ["BQ_DATASET"]
+
+
+
+
+
+
+
+##################  CONSTANTS  #####################
+LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), "code", "JeremyVandercam", "data")
