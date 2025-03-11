2d2a0f1987fdd1e44369b79deab984a3f70daf3f
Maximilian Gust <@users.noreply.github.com>
2025-03-11 17:24:19 +0100

    updated train_model function

--- ml_logic/model.py
+++ ml_logic/model.py
@@ -1,39 +1,32 @@
 import xgboost as xgb
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+import pandas as pd
+import numpy as np
 
 
-class XGBTrainer:
-    def __init__(self, params):
-        self.params = params
 
-    def fit(self, dtrain, dval, y_val):
-        print("Training XGB model")
+def train_model(X:pd.DataFrame, y:pd.DataFrame) ->np.array|float:
 
-        # Add evaluation metric and early stopping rounds to parameters
-        early_stopping_rounds = 20
-        evals = [(dtrain, "train"), (dval, "eval")]
+   # Split data into train, test and validation sets
+    X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.3, random_state = 42)  # TEST = 30%
 
-        # Train the model with early stopping
-        model = xgb.train(
-            self.params,
-            dtrain,
-            num_boost_round=1590,
-            evals=evals,
-            early_stopping_rounds=early_stopping_rounds,
-        )
+    # Use the same function above for the validation set
+    X_test, X_val, y_test, y_val = train_test_split(
+    X_test, y_test, test_size = 0.3, random_state = 42)  # TEST = 15%
 
-        # Predict and calculate AUC
-        y_pred = model.predict(dval, iteration_range=(0, model.best_iteration + 1))
-        auc = roc_auc_score(y_val, y_pred)
+    xgb_cl = XGBClassifier(max_depth=10, n_estimators=100, learning_rate=0.1,eval_metric="auc")
 
-        print(f"---AUC: {auc:.6f}")
+    xgb_cl.fit(X_train, y_train,
+    # evaluate loss at each iteration
+    eval_set=[(X_train, y_train), (X_val, y_val)],
+    # stop iterating when eval loss increases 5 times in a row
+    early_stopping_rounds=5)
 
-        return auc
-params = {
-        "objective": "binary:logistic",
-        "n_jobs": -1,
-        "verbosity": 0
-        "eval_metric": "auc",
-        "random_state": SEED,
-    }
-auc = XGBTrainer(params).fit(dtrain, dval, y_val)
+    y_pred = xgb_cl.predict(X_test)
+
+    score = roc_auc_score(y_test,y_pred)
+
+    return y_pred, score
