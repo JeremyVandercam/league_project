e6d5fbf45d68e0987789b0f23f298c1aff4151f3
Maximilian Gust <@users.noreply.github.com>
2025-03-18 12:03:01 +0100

    Motion tracking trained first model locally

---
+++ league/motion_tracking/params.py
@@ -0,0 +1,9 @@
+import os
+
+LOCAL_DATA_PATH = os.path.join(os.path.expanduser("~"), ".league_project", "data")
+NUM_EPOCHS = int(os.environ["EPOCHS"])
+COMET_API_KEY = os.environ["COMET_API_KEY"]
+COMET_PROJECT_NAME = os.environ["COMET_PROJECT_NAME"]
+COMET_MODEL_NAME = os.environ["COMET_MODEL_NAME"]
+COMET_WORKSPACE_NAME = os.environ["COMET_WORKSPACE_NAME"]
+ROBOFLOW_API_KEY = os.environ["ROBOFLOW_API_KEY"]
