b6282a1b891b50196b6d805f28598e0dd04cf316
Polovinkin Nikita <poloniki@ya.ru>
2025-03-18 23:38:44 +0100

    fixed the dockerfile issues

--- league/motion_tracking/train.py
+++ league/motion_tracking/train.py
@@ -3,6 +3,7 @@ from league.motion_tracking.params import *
 import os
 import comet_ml
 from comet_ml import API
+from league.motion_tracking.data import load_data
 
 
 # Function to train the model
@@ -42,16 +43,19 @@ def train_model(epochs: int = 10, img_size: int = 640):
     # If loading pretrained weights fails, initialize a new model
     except Exception as error:
         print(f"❌ Could not load weights: {error}")
+        params = {
+            "data": os.path.join(LOCAL_DATA_PATH, "data.yaml"),
+            "epochs": epochs,
+            "imgsz": img_size,
+            "patience": 20,
+        }
+        device = os.environ.get("DEVICE", "CUDA")
+        if device == "mps":
+            params.update({"device": "mps"})
 
         # Initialize a new YOLO model with default weights
-        model = YOLO("yolo11s.pt")
-        model.train(
-            data=os.path.join(LOCAL_DATA_PATH, "data.yaml"),
-            epochs=epochs,
-            imgsz=img_size,
-            patience=20,
-            device="mps",
-        )
+        model = YOLO("yolo11m.pt")
+        model.train(**params)
 
     # Save the trained model weights to Comet ML
     experiments = api.get(
@@ -68,12 +72,12 @@ def train_model(epochs: int = 10, img_size: int = 640):
 
     # Sort list of experiments by one of the metrics to find best one
     experiments.sort(
-        key=lambda each_experiment: float(
-            each_experiment.get_metrics_summary("metrics/mAP50(B)")["valueMax"]
+        key=lambda each_experiment: (
+            float(each_experiment.get_metrics_summary("metrics/mAP50(B)")["valueMax"])
+            # If some experiment got stopped without any metric we want to skip it
+            if isinstance(each_experiment.get_metrics_summary("metrics/mAP50(B)"), dict)
+            else 0
         )
-        # If some experiment got stopped without any metric we want to skip it
-        if isinstance(each_experiment.get_metrics_summary("metrics/mAP50(B)"), dict)
-        else 0
     )
 
     # get best experiment
@@ -90,4 +94,5 @@ def train_model(epochs: int = 10, img_size: int = 640):
 
 # Main execution
 if __name__ == "__main__":
+    load_data()
     train_model(epochs=NUM_EPOCHS)
